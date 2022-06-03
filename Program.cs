5d4faf2d4ffabe770573f895975353ae29551e4a
3109724 <3109724@TE-GDA-D-D37H08>
2022-06-03 11:43:42 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace zgaduj
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int r, g;
+		
+			Random rand = new Random();
+			int min = 1;
+			int max = 10;
+			r = rand.Next(min, max);
+			Console.WriteLine("Zgadnij liczbe, bo wpierdol");
+			do
+			{
+				g = Convert.ToInt32(Console.ReadLine());
+				if (r != g)
+					Console.WriteLine(((r < g) ? "za duza" : "za mala"));
+			} while (r != g);
+			Console.WriteLine("zgadles");
+			Console.ReadKey();
+		}
+	}
+}
